6cd48436d9968e23b1b0d6871e2920c60935b624
ArbitechSolution <arbitechsolutions@gmail.com>
2022-10-31 10:32:18 +0500

    todo-app

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import TodoForm from './component/TodoForm';
+import TodoList from './component/TodoList';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Todo from './component/Todo/Todo';
+function App() {
+  return (
+    <div className="App">
+      <Todo/>
+      {/* <TodoList/> */}
+    {/* <TodoForm/> */}
+    </div>
+  );
+}
+
+export default App;
